d806e2d05a7dcc63303006d01b68d4213ec262d8
ndjana30 <davyndjana@gmail.com>
2024-04-02 20:56:28 +0200

    new commit

--- src/main/java/com/limiter/demo/DemoApplication.java
+++ src/main/java/com/limiter/demo/DemoApplication.java
@@ -10,7 +10,7 @@ import org.springframework.web.client.RestTemplate;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		// SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 	}
 	@Bean
 	public RestTemplate restTemplate() {
