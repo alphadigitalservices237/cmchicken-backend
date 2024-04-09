3039c9a094e69db3910a5fa85b0a74ff1132960c
ndjana30 <davyndjana@gmail.com>
2024-04-09 15:32:42 +0200

    new commit

--- src/main/java/com/limiter/demo/rest/controllers/PurchasedObjectController.java
+++ src/main/java/com/limiter/demo/rest/controllers/PurchasedObjectController.java
@@ -7,13 +7,17 @@ import com.limiter.demo.repositories.ProductRepository;
 import com.limiter.demo.repositories.PurchaseObjectRepo;
 import com.limiter.demo.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.*;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("api/v1/auth")
@@ -25,6 +29,8 @@ public class PurchasedObjectController {
     private ProductRepository productRepository;
     @Autowired
     private UserRepository userRepository;
+
+
     @GetMapping("products/all")
     public Object getUserProducts()
     {
@@ -37,6 +43,48 @@ public class PurchasedObjectController {
                 }
                 else {
                     return new ResponseEntity<>(client.get().getPurchaseobjectList(), HttpStatus.OK);
+                }
+        }
+        else{
+            return new ResponseEntity<>("PLEASE LOGIN", HttpStatus.UNAUTHORIZED);
+        }
+    }
+
+    @GetMapping("products/{date}/all")
+    public Object getUserProductsPerDay(@PathVariable @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate date)
+    {
+        Authentication auth  = SecurityContextHolder.getContext().getAuthentication();
+        Optional<UserEntity> client = userRepository.findByUsername(auth.getName());
+        if(client.isPresent())
+        {
+                if (client.get().getPurchaseobjectList().isEmpty()) {
+                    return new ResponseEntity<>("NO ITEMS PURCHASED YET", HttpStatus.EXPECTATION_FAILED);
+                }
+                else {
+                    List<Purchaseobject> objs = client.get().getPurchaseobjectList().stream().filter(obj->obj.getAddedDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate().equals(date))
+                    .collect(Collectors.toList());
+                    return new ResponseEntity<>(objs, HttpStatus.OK);
+                }
+        }
+        else{
+            return new ResponseEntity<>("PLEASE LOGIN", HttpStatus.UNAUTHORIZED);
+        }
+    }
+
+    @GetMapping("products/{date}/today")
+    public Object getUserProductsForToday()
+    {
+        Authentication auth  = SecurityContextHolder.getContext().getAuthentication();
+        Optional<UserEntity> client = userRepository.findByUsername(auth.getName());
+        if(client.isPresent())
+        {
+                if (client.get().getPurchaseobjectList().isEmpty()) {
+                    return new ResponseEntity<>("NO ITEMS PURCHASED YET", HttpStatus.EXPECTATION_FAILED);
+                }
+                else {
+                    List<Purchaseobject> objs = client.get().getPurchaseobjectList().stream().filter(obj->obj.getAddedDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate().equals(LocalDate.now()))
+                    .collect(Collectors.toList());
+                    return new ResponseEntity<>(objs, HttpStatus.OK);
                 }
         }
         else{
